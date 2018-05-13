cd4f2c09346efb27e32aeab1820eb9bcd7e585a7
Brett Cimbalik <bcimbali@gmail.com>
2018-05-12 21:30:58 -0500

    added catch all route to html routes page

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,11 +14,19 @@ module.exports = function(app){
 	// In each of the below cases the user is shown an HTML page of content
 // ---------------------------------------------------------------------------
 
+// Route for home
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./../public/home.html"));
 });
 
+// Route for survey
 app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname, "./../public/survey.html"));
 });
+
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
 }
